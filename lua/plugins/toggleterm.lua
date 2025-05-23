b48509e148d7c983b5b308dc894f52fd31b09109
François Chaix <fr.chaix@gmail.com>
2025-05-23 20:57:48 +0200

    fix erreur de merge

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -6,30 +6,24 @@ return {
       -- Détection du système d'exploitation
       local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
 
-  --{'akinsho/toggleterm.nvim', version = "*", config = true}
-  -- or
-  {'akinsho/toggleterm.nvim', version = "*", opts = {
-    size = 20,
-    open_mapping = [[<leader>tt]],
-    hide_numbers = true, -- hide the number column in toggleterm buffers
-    direction = 'float',
-    float_opts = {
-      border = 'curved', -- 'curved' | 'double' | 'shadow' | 'none' | 'single' | 'rounded' | 'solid' 
-      -- width = 100,
-      -- height = 20,
-      winblend = 3,
-        highlights = {
-            border = 'Normal',
-            background = 'Normal',
-        },
-    },
-  }}
+      -- Définir la commande du shell en fonction de l'OS
+            local shell_cmd = is_windows
+            and [[powershell.exe -NoLogo -NoExit -Command ". 'C:\Users\fhc\psProfile.ps1'"]]
+            or "zsh"
 
             return {
                 size = 20,
                 open_mapping = [[<leader>tt]],
-                hide_numbers = true,
+                hide_numbers = false, -- hide the number column in toggleterm buffers
                 direction = 'float',
+                float_opts = {
+                    border = 'curved',
+                    winblend = 3,
+                    highlights = {
+                        border = 'Normal', 
+                        background = 'Normal',
+                    },
+                },
                 -- Configuration du terminal par défaut
                 shell = shell_cmd,
             }
