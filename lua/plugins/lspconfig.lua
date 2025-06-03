792caa02c534812847627a05f7c8741354f1a1c7
François Chaix <francois.chaix@itga.fr>
2025-06-03 16:23:10 +0200

    Fixes omnisharp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -55,20 +55,41 @@ return {
     })
 
 
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    capabilities.textDocument.completion.completionItem.resolveSupport = {
+      properties = {
+        "documentation",
+        "detail",
+        "additionalTextEdits",
+      }
+    }
+
     -- Configuration de LSP pour C# -->  Déjà configuré dans csharp.lua
     lspconfig.omnisharp.setup{
       cmd = { "omnisharp", "--languageserver" , "--hostPID", tostring(vim.fn.getpid()) },
+      filetypes = { "cs", "vb" },
+      root_dir = lspconfig.util.root_pattern("*.sln", "*.csproj", ".git"),
+      settings = {
+        omnisharp = {
+          enableRoslynAnalyzers = true,
+          organizeImportsOnFormat = true,
+          organizeImportsOnSave = true,
+          enableEditorConfigSupport = true,
+          sdkIncludePrerelease = true,
+        },
+      },
       on_attach = function(client, bufnr)
         local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
         local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-    --
-    --     -- Enable completion triggered by <c-x><c-o>
+        --
+        --     -- Enable completion triggered by <c-x><c-o>
         buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-    --
-    --     -- Mappings.
+        --
+        --     -- Mappings.
         local opts = { noremap=true, silent=true }
-    --
-    --     -- See `:help vim.lsp.*` for documentation on any of the below functions
+        --
+        --     -- See `:help vim.lsp.*` for documentation on any of the below functions
         buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
         buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
         buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
@@ -76,15 +97,15 @@ return {
         buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
         buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
         buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    --
+        --
         buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
         buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
         buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    --
+        --
         buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
         buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
         buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    --
+        --
         buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
         buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
         buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
