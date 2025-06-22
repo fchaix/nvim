21d9b6e6defa5e5ca09fc8caee75a2033cd8247a
François Chaix <fr.chaix@gmail.com>
2025-06-22 19:15:49 +0200

    Copilot stuff

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,13 @@
 return {
   'github/copilot.vim',
   config = function()
+    vim.g.copilot_enabled = false
     vim.g.copilot_assume_mapped = true
     vim.g.copilot_no_tab_map = true
     vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { expr = true, silent = true })
-    vim.api.nvim_set_keymap('i', '<C-K>', 'copilot#Previous()', { silent = true })
-    vim.api.nvim_set_keymap('i', '<C-L>', 'copilot#Next()', { silent = true })
-    vim.api.nvim_set_keymap('i', '<C-E>', 'copilot#Dismiss()', { silent = true })
-  end
-}
+    vim.api.nvim_set_keymap('i', '<C-K>', 'v:lua.copilot_previous()', { expr = true, silent = true })
+    vim.api.nvim_set_keymap('i', '<C-L>', 'v:lua.copilot_next()', { expr = true, silent = true })
+    vim.api.nvim_set_keymap('i', '<C-E>', 'v:lua.copilot_dismiss()', { expr = true, silent = true })
 
+    end
+}
