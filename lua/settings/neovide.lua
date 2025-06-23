f03fc10c084892741038df3b5ace3109e1548209
François Chaix <francois.chaix@itga.fr>
2025-06-23 12:03:06 +0200

    desactivation de codeium pour le moment

--- lua/settings/neovide.lua
+++ lua/settings/neovide.lua
@@ -36,3 +36,20 @@ vim.g.neovide_remember_window_size = true
 vim.keymap.set("n", "<F11>", function()
   vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
 end, { desc = "Toggle fullscreen in Neovide" })
+
+
+-- Copy paste and stuff
+if vim.g.neovide then
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
