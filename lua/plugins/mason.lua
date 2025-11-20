eb23b679d4a42417914049707cc45a6c204b43b5
François Chaix <fr.chaix@gmail.com>
2025-11-20 18:15:29 +0100

    fix mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,29 +1,23 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    registries = {
-      "github:mason-org/mason-registry",
-      "github:Crashdummyy/mason-registry",
-    },
-    ensure_installed = {
-      "lua-language-server",
-
-      "xmlformatter",
-      "csharpier",
-      "prettier",
-
-      "stylua",
-      "html-lsp",
-      "css-lsp",
-      "json-lsp",
-
-      "pyright",
-
-      -- !
-      "roslyn-unstable",
-      -- "rzls", -- razor lsp
-      -- "csharp-language-server",
-      -- "omnisharp",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+      ensure_installed = {
+        "lua-language-server",
+        "xmlformatter",
+        "csharpier",
+        "prettier",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "json-lsp",
+        "pyright",
+        "roslyn-unstable",
+      },
     },
   },
   {
