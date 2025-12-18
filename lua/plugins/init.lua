beacb10f20ea4f2c8e285cb28332f7511098d4f7
François Chaix <francois@chaix.fr.eu.org>
2025-12-18 19:25:02 +0100

    Tiny inline diags

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -42,6 +42,7 @@ require("lazy").setup({
   ------------------------------------------------------------
   require('plugins.lsp'),
   require('plugins.roslyn'),      -- LSP C#
+  require('plugins.tiny-inline-diagnostics'), -- Diagnostics inline légers
   require('plugins.csharp'),      -- Améliorations C#
   require('plugins.cmp'),         -- Completions
   require('plugins.luasnip'),     -- Snippets
