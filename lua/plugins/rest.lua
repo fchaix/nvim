f7a275e214ae1b1762803ea77e9c9eb7e3e1bf1a
François Chaix <francois.chaix@itga.fr>
2026-01-23 16:58:49 +0100

    add leader rr pour run REST request in .http file

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -6,5 +6,13 @@ return {
       opts.ensure_installed = opts.ensure_installed or {}
       table.insert(opts.ensure_installed, "http")
     end,
+
+    -- Horkeys
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>rr",
+      "<cmd>Rest run<cr>",
+      { noremap = true, silent = true, desc = "Run REST request under cursor" }
+    ),
   }
 }
