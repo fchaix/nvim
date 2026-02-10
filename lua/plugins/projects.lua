90006535de444e8e3d010bc058976ccb70daa22e
François Chaix <francois.chaix@itga.fr>
2026-02-10 16:38:56 +0100

    virgule manquante et projet bddlims

--- lua/plugins/projects.lua
+++ lua/plugins/projects.lua
@@ -10,19 +10,15 @@ return {
       "C:/Users/fhc/source/repos/image.webapi",
       "C:/Users/fhc/source/repos/tools",
       "C:/Users/fhc/source/repos/timetrack",
+      "C:/Users/fhc/source/repos/bddlims",
       "C:/Users/fhc/AppData/Local/nvim",
       "C:/Users/fhc/kanata",
       "//wsl.localhost/Ubuntu/home/fhc/eeznuts",
-<<<<<<< HEAD
-      "//wsl.localhost/Ubuntu/home/fhc/comp/offpunk"
-||||||| 4aa655b
-      "//wsl.localhost/Ubuntu/home/fhc/eeznuts"
-=======
+      "//wsl.localhost/Ubuntu/home/fhc/comp/offpunk",
 
       -- Projects on perso linux machines
       "~/.config/nixos-config",
       "~/.config/nvim"
->>>>>>> 4eb1dc4bb3425d3f0d47e33397bb29a2548160ab
     },
     patterns = { ".git", "package.json", "*.sln", "Makefile" }, -- détection auto
     project_dir = "~/neovim_projects", -- où sauvegarder les sessions
