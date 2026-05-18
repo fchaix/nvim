84254236a504fb20469243f3d915b25ee43a119e
François Chaix <francois.chaix@itga.fr>
2026-05-18 16:59:43 +0200

    fix: install http parser pre-compiled from legacy lazy-rocks to avoid treesitter error

--- lua/config/plugins/rest.lua
+++ lua/config/plugins/rest.lua
@@ -1,8 +1,28 @@
 local helper = require("config.plugins.helpers")
 
+local function copy_existing_parser(source, target)
+  if vim.fn.filereadable(target) == 0 and vim.fn.filereadable(source) == 1 then
+    vim.fn.mkdir(vim.fn.fnamemodify(target, ":h"), "p")
+    vim.fn.system({ "cp", source, target })
+  end
+end
+
+local function ensure_http_parser()
+  local parser_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "site", "pack", "core", "opt", "nvim-treesitter", "parser")
+  local parser_file = vim.fn.has("win32") == 1 and "http.dll" or "http.so"
+  local target = vim.fs.joinpath(parser_dir, parser_file)
+  if vim.fn.filereadable(target) == 1 then
+    return
+  end
+  local old_parser = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy-rocks", "rest.nvim", "lib", "lua", "5.1", "parser", parser_file)
+  copy_existing_parser(old_parser, target)
+end
+
 local M = {}
 
 function M.setup()
+  ensure_http_parser()
+
   helper.safe_require("rest-nvim", function(rest)
     rest.setup({
       result = {
@@ -14,14 +34,6 @@ function M.setup()
     })
 
     vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<CR>", { desc = "Run REST request" })
-
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "http",
-      callback = function(args)
-        pcall(vim.treesitter.language.add, "http")
-        vim.bo[args.buf].commentstring = "# %s"
-      end,
-    })
   end)
 end
 
